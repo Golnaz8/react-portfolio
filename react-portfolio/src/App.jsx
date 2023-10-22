88d7ec3389f160045a616c9392e1c20a894f0ec8
Golnaz8 <golnaz.brj@gmail.com>
2023-10-22 11:37:21 -0400

    added footer

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -2,15 +2,25 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
 import './styles/App.css';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
       <Nav />
-      <main className="mx-3">
+      <main className="mx-3">        
         <Outlet />
       </main>
+      <footer style={{ display: 'flex', justifyContent: 'center', marginTop: '10%', paddingBottom:'3%' }}>
+        <a style={{marginRight: '2%'}} href="https://github.com/golnaz8" target="_blank" rel="noopener noreferrer">
+          <FaGithub size={30} />
+        </a>
+        <a href="https://www.linkedin.com/in/naz-brj" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin size={30} />
+        </a>
+      </footer>
     </>
   );
 }
